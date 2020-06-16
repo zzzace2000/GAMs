beb2cf186ff389b9e2125045194f05fac45c1a5e
Kingsley Chang <zzzace2000@gmail.com>
2020-06-16 14:25:05 -0400

    Add my_interpret/ package

---
+++ my_interpret/python/interpret/perf/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2019 Microsoft Corporation
+# Distributed under the MIT software license
+
+from .curve import ROC, PR  # noqa: F401
+from .regression import RegressionPerf  # noqa: F401
---
+++ my_interpret/python/interpret/utils/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2019 Microsoft Corporation
+# Distributed under the MIT software license
+
+from .all import *  # noqa: F401,F403
+from .distributed import *  # noqa: F401,F403
